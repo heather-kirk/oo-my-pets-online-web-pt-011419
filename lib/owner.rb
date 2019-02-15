e14d5b2085edd4dd0408f12d35262a1a3953e94e
Heather Kirk <hskirk@gmail.com>
2019-02-15 02:32:03 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -55,8 +55,8 @@ def feed_fish
 end 
 
 def sell_pets
-  pets.each do |type, pets| 
-    pets.each do |pet|
+  @pets.each do |type, owners_pets| 
+    owners_pets.each do |pet|
       pet.mood = "nervous"
   end 
 end 
