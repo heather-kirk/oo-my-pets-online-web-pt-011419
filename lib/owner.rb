4a4034375c1aa9192448c4b24a74c5852f7667ef
Heather Kirk <hskirk@gmail.com>
2019-02-15 02:20:11 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -28,9 +28,17 @@ def say_species
   "I am a #{@species}."
 end 
  
-def buy_fish(fish)
+def buy_fish(name)
+  fish = Fish.new(name)
   
 end 
- 
+
+def buy_cat(name)
+  cat = Cat.new(name)
+  pets[:cats] << cat 
+end 
+
+def buy_dog(name)
+  dog = Dog.new(name)
  
 end
