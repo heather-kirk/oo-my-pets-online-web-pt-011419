ccfbb80235df44e3962efdb3557387f3e5d8aadb
Heather Kirk <hskirk@gmail.com>
2019-02-15 02:44:26 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -59,6 +59,8 @@ def sell_pets
   pets.each do |type, pets| pets.each do |pet| pet.mood = "nervous"
   end 
 end 
+pets.clear
+end 
 
 def list_pets 
   "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
